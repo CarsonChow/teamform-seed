fec8e0aa7d0b990c31cf070ccb9a4eed6d190c76
CarsonChow <ccchowae@connect.ust.hk>
2016-11-15 17:49:52 +0800

    Add files via upload

--- app/unit_tests/test_chatRoom.js
+++ app/unit_tests/test_chatRoom.js
@@ -13,6 +13,11 @@ describe('chatRoom test', function() {
 	
 	describe('chat room test', function() {
 		it('addMessage', function() {
+			$scope.input.message = "whatever";
+			$scope.addMessage();
+			expect($scope.chatList.message).toBe("whatever");
+			expect($scope.chatList.userName).toBe("visitor");
+			expect($scope.chatList.date).toBeDefined();
 		});
 		
 	});
